ab9c870cf380df400be7b1b6cf8f3d0d6e63e971
Angel Valadez <valadezhdzangb@gmail.com>
2024-09-20 10:27:08 -0600

    Añadi el js

---
+++ script.js
@@ -0,0 +1,31 @@
+alert("Hola mundo");
+/* 
+18/09/2024 / Valadez Hernandez Angel Benjamin
+
+*/
+
+// Declaracion de variables
+let nombre = "Angel";
+const PI = 3.1416;
+var edad = 25;
+
+/*Tipos de datos
+De texto (String): "Hola" , 'mundo'
+Numero (number): 5,3,14
+Booleano (boolean): true. false
+*/
+
+//Ejemplo
+let mensaje = "Hola mundo";
+let numero = 42;
+let estudiante = true;
+
+
+//Operaciones matematicas
+let a = 5;
+let b = 2;
+
+let suma = a+b;
+let resta = a-b;
+let multi = a*b;
+let division = a/b;
