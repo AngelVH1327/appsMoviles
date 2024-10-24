bcc69bd7a4031dd8489f829e8a639cdd60f6d533
angelvh <valadezhdzangb@gmail.com>
2024-10-23 20:39:07 -0600

    clase de hoy

---
+++ CLASE_23_10_2024/script.js
@@ -0,0 +1,19 @@
+let  task = ["comprar", "estudiar", "limpiar la casa"];
+
+console.log ("lista de tareas", task);
+
+task.push("ir a la escuela");
+
+task.splice(3,1);
+
+task.push("ir a casa");
+
+//******************************* */
+
+let tasks = ["comprar","estuidiar", "limpiar casa", "ir a la escuela"];   
+
+    tasks.forEach((task,index) => {
+        console.log(`Tarea + ${index} ${task}`);
+    })
+
+
